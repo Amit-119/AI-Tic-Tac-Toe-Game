3dfe6ace149e1d09c368e1134c2f090931f30c21
Amit Singh <amitdmk908@gmail.com>
2021-02-13 19:18:02 +0530

    Update TicTacToeAI.py

--- TicTacToeAI.py
+++ TicTacToeAI.py
@@ -1,7 +1,15 @@
-import random as rd
+#--------- Developed by Amit Singh ----------
+#--------- NIT Manipur ----------------------
 
+
+
+
+#Status of the Board, Initially empty
 theBoard = [None,' ',' ',' ',' ',' ',' ',' ',' ',' ']
+computer,opponent = "X","O"
 
+
+#Function to print the current status of Board
 def printBoard(board):
     print(board[1] + '|' + board[2] + '|' + board[3])
     print('-+-+-')
@@ -10,7 +18,110 @@ def printBoard(board):
     print(board[7] + '|' + board[8] + '|' + board[9])
 
 
-def wins(board):
+
+#Function to check if any moves left in Board
+def MovesLeft(Board):
+    for box in Board:
+        if box == " ":
+            return True
+    return False
+
+
+#Evaluation function of the Board for next move
+def Evaluate(board):
+    for x in [1,4,7]:
+        if board[x]==board[x+1]==board[x+2]:
+            if board[x]==computer:
+                return 100
+            elif board[x]==opponent:
+                return -100
+    for y in [1,2,3]:
+        if board[y]==board[y+3]==board[y+6]:
+            if board[y]==computer:
+                return 100
+            elif board[y]==opponent:
+                return -100
+    if board[1]==board[5]==board[9]:
+        if board[5]==computer:
+            return 100
+        elif board[5]==opponent:
+            return -100
+    if board[3]==board[5]==board[7]:
+        if board[5]==computer:
+            return 100
+        elif board[5]==opponent:
+            return -100
+    return 0
+
+
+
+#Minimax function to evaluate score
+def MiniMax(board,depth,isMax):
+    score = Evaluate(board)
+    if score == 100 or score == -100:
+        return score
+
+    if not MovesLeft(board):
+        return 0
+
+    if isMax:
+        best = -10000
+        for i in range(1,10):
+            if board[i]==" ":
+                board[i] = computer
+                best = max(best,MiniMax(board,depth+1,not isMax)) - depth
+                board[i] = " "
+        return best
+    
+    else:
+        best = 10000
+        for i in range(1,10):
+            if board[i]==" ":
+                board[i] = opponent
+                best = min(best,MiniMax(board,depth+1,not isMax)) + depth
+                board[i] = " "
+        return best
+
+
+
+#Function to find the next Best move for Computer
+def FindBestMove(board):
+    bestVal = -10000
+    index = -1
+    for i in range(1,10):
+        if board[i] == " ":
+            board[i] = computer
+            reward = MiniMax(board,0,False)
+            board[i] = " "
+            if reward>bestVal:
+                bestVal = reward
+                index = i
+    #print("The maxinum reward is :",bestVal)
+    #You can uncomment the above line to see the reward
+    #for next move
+    return index
+
+
+
+#Function to check if move given by player is valid
+#This is for smart intellectuals who intentionally give wrong input :)
+def isValidMove(board,move):
+    if not move.isdigit():
+        print("Please enter a number from 1 to 9.\n")
+        return False
+    move = int(move)
+    if move<1 or move>9:
+        print("Please enter a number from 1 to 9.\n")
+        return False
+    if board[move]!=" ":
+        print("That index is taken please select a valid index.\n")
+        return False
+    return True
+
+
+
+#Checking if anyone have won the game at rurntime
+def Wins(board):
     if board[1]==board[2]==board[3] and board[1] != ' ':
         return True
     if board[1]==board[4]==board[7] and board[1] != ' ':
@@ -29,171 +140,53 @@ def wins(board):
         return True
     return False
 
-def choose(board):
-    #attacking the player's mistake
-    a = [board[1],board[5],board[9]]
-    b = [1,5,9]
-    if a.count('O')==2 and ' ' in a:
-        return b[a.index(' ')]
-    a = [board[3],board[5],board[7]]
-    b = [3,5,7]
-    if a.count('O')==2 and ' ' in a:
-        return b[a.index(' ')]
-    a = [board[2],board[5],board[8]]
-    b = [2,5,8]
-    if a.count('O')==2 and ' ' in a:
-        return b[a.index(' ')]
-    a = [board[4],board[5],board[6]]
-    b = [4,5,6]
-    if a.count('O')==2 and ' ' in a:
-        return b[a.index(' ')]
-    a = [board[1],board[2],board[3]]
-    b = [1,2,3]
-    if a.count('O')==2 and ' ' in a:
-        return b[a.index(' ')]
-    a = [board[1],board[4],board[7]]
-    b = [1,4,7]
-    if a.count('O')==2 and ' ' in a:
-        return b[a.index(' ')]
-    a = [board[3],board[6],board[9]]
-    b = [3,6,9]
-    if a.count('O')==2 and ' ' in a:
-        return b[a.index(' ')]
-    a = [board[7],board[8],board[9]]
-    b = [7,8,9]
-    if a.count('O')==2 and ' ' in a:
-        return b[a.index(' ')]
-    
-    #defence for players making pair of 3 X's
-    a = [board[1],board[5],board[9]]
-    b = [1,5,9]
-    if a.count('X')==2 and ' ' in a:
-        print(b)
-        return b[a.index(' ')]
-    a = [board[3],board[5],board[7]]
-    b = [3,5,7]
-    if a.count('X')==2 and ' ' in a:
-        print(b)
-        return b[a.index(' ')]
-    a = [board[2],board[5],board[8]]
-    b = [2,5,8]
-    if a.count('X')==2 and ' ' in a:
-        print(b)
-        return b[a.index(' ')]
-    a = [board[4],board[5],board[6]]
-    b = [4,5,6]
-    if a.count('X')==2 and ' ' in a:
-        print(b)
-        return b[a.index(' ')]
-    a = [board[1],board[2],board[3]]
-    b = [1,2,3]
-    if a.count('X')==2 and ' ' in a:
-        print(b)
-        return b[a.index(' ')]
-    a = [board[1],board[4],board[7]]
-    b = [1,4,7]
-    if a.count('X')==2 and ' ' in a:
-        print(b)
-        return b[a.index(' ')]
-    a = [board[3],board[6],board[9]]
-    b = [3,6,9]
-    if a.count('X')==2 and ' ' in a:
-        print(b)
-        return b[a.index(' ')]
-    a = [board[7],board[8],board[9]]
-    b = [7,8,9]
-    if a.count('X')==2 and ' ' in a:
-        print(b)
-        return b[a.index(' ')]
-    
-    #strategy to counter the player
-    if board[5] == ' ':
-        return 5
-    elif board[5] == 'X':
-        option = [1,3,7,9]
-        for i in option:
-            if board[i] != ' ':
-                option.remove(i)
-        print("Options:",option)
-        if len(option)!=0:
-            return (rd.choice(option))
-    
-    if board[1] == 'X':
-        if board[9] == ' ':
-            return 9
-        else:
-            option = [3,7]
-            for i in option:
-                if board[i] != ' ':
-                    option.remove(i)
-            print("Options:",option)
-            if len(option)!=0:
-                return (rd.choice(option))
-    if board[3] == 'X':
-        if board[7] == ' ':
-            return 7
-        else:
-            option = [1,9]
-            for i in option:
-                if board[i] != ' ':
-                    option.remove(i)
-            print("Options:",option)
-            if len(option)!=0:
-                return (rd.choice(option))
-    if board[7] == 'X':
-        if board[3] == ' ':
-            return 3
-        else:
-            option = [1,9]
-            for i in option:
-                if board[i] != ' ':
-                    option.remove(i)
-            print("Options:",option)
-            if len(option)!=0:
-                return (rd.choice(option))
-    if board[9] == 'X':
-        if board[1] == ' ':
-            return 1
-        else:
-            option = [3,7]
-            for i in option:
-                if board[i] != ' ':
-                    option.remove(i)
-            print("Options:",option)
-            if len(option)!=0:
-                return (rd.choice(option))
-    #finally useless moves for formality
-    print(board.index(' '))
-    return board.index(' ')
 
 
-print("You have 'X' and I have 'O'")
+#Utilize all the functions and Plays the game for once
+def PlayGame(board):
+    while " " in board:
+        while True:
+            move = input("Enter your move : ")
+            if isValidMove(board,move):
+                move = int(move)
+                board[move] = "O"
+                break
+        printBoard(board)
+        print("\n")
+        if Wins(board):
+            print("Congrats!! You won.  :)")
+            break
 
-while ' ' in theBoard:
-    turn = 'X'
-    printBoard(theBoard)
-    print("\nYour turn. Move on which index? ")
-    move = int(input())%10
-    if theBoard[move] != ' ':
-        print("That position is already occupied.")
-        continue
-    theBoard[move] = turn
-    printBoard(theBoard)
-    if wins(theBoard):
-        print("Congrats! You won.")
-        break
-    if ' ' not in theBoard:
-        break
-    turn = 'O'
-    move = choose(theBoard)
-    print("\nI move on index ",move)
-    theBoard[move] = turn
-    if wins(theBoard):
-        print("Oops... I won :):)")
-        break
+        if " " not in board:
+            break
+        Comp_move = FindBestMove(board)
+        print("I choose",Comp_move)
+        board[Comp_move] = "X"
+        printBoard(board)
+        print("\n")
+        if Wins(board):
+            print("OOps!  I won.  :)")
+            break
 
-printBoard(theBoard)
+
+#Driver code
+if __name__=="__main__":
+    print("\nYou have 'O' and I have 'X' ")
+    indexBoard = [None,'1','2','3','4','5','6','7','8','9']
+    print("Select your move according to the index given: ")
+    printBoard(indexBoard)
+    print("\n")
+
+    board = theBoard
+    while True:
+        PlayGame(board)
+        wish = input("Want to play more?  Press N to exit:  ")
+        if wish=="N" or wish=="n":
+            break
+        board = [None,' ',' ',' ',' ',' ',' ',' ',' ',' ']
+        printBoard(board)
+        print()
 
 
 
-    
+#----------- Developed by Amit Singh --------------------------
